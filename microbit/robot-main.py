1ef0060fae4761ed995b85f033a79dd5d4c724ec
thechrislewis <chrislewis64@gmail.com>
2025-09-15 14:57:28 +0100

    added some robot code

--- microbit/robot-main.py
+++ microbit/robot-main.py
@@ -17,7 +17,7 @@ class Robot:
 
         # setup leds
         self.pixel_count = 5
-        self.leds = neopixel.NeoPixel(pin0, self.pixel_count)
+        self.leds = NeoPixel(pin0, self.pixel_count)
         self.leds[0] = (255,0,0)
         self.leds[1] = (0,255,0)
         self.leds[2] = (0,0,0)
@@ -28,8 +28,8 @@ class Robot:
         #setup servos
         pin1.set_analog_period(20)
         pin2.set_analog_period(20)
-        pin1.write_analog(90) # stop
-        pin2.write_analog(90) # stop
+        pin1.write_analog(0) # stop
+        pin2.write_analog(0) # stop
         
         display.show(Image.HEART)
 
@@ -41,30 +41,50 @@ class Robot:
     def fwd(self):
         display.show(Image.ARROW_N)
         pin1.write_analog(180) # full forward
-        pin2.write_analog(0)   # full forward
+        pin2.write_analog(180)   # full forward
 
     def back(self):
-        display.show(Image.ARROW_S)
+       
         
     def left(self):
-        display.show(Image.ARROW_W)
-        
+        pass:
+    
     def right(self):
-        display.show(Image.ARROW_E)
+        pass:
         
     def stop(self):
         display.show(Image.SQUARE)
         pin1.write_analog(0) # stop
         pin2.write_analog(0) # stop
+        self.setLeds(0,0,0)  # when we control leds inside object then use self
 
 # End of robot.py
 
 #test code
 if __name__ == "__main__":
 
-    robbie = Robot()
-    robbie.fwd()
-    robbie.back()
-    robbie.left()
-    robbie.right()
-    robbie.stop()
+    robbie = Robot() # create robot 'instance'
+
+    run_code = False # don't run robot until button pressed
+
+    while True: # loop forever
+        if button_a.is_pressed():
+            run_code = True
+            # when we control leds outside object, we use Robot 'instance'
+            robbie.setLeds(128,128,128) 
+        if button_b.is_pressed():
+            run_code = False
+            robbie.stop()
+
+        # the code below takes at least 4 seconds to run before a button press can be checked...
+        if run_code: 
+            robbie.fwd()
+            sleep(1000)
+            robbie.back()
+            sleep(1000)
+            robbie.left()
+            sleep(1000)
+            robbie.right()
+            sleep(1000)
+            robbie.stop()
+            run_code = False
